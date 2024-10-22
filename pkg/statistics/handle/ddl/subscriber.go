7e0d127e816db5ef1fb7f224689a26277b8025c1
lance6716 <lance6716@gmail.com>
2024-10-22 19:25:50 +0800

    fix new type
    Signed-off-by: lance6716 <lance6716@gmail.com>

--- pkg/statistics/handle/ddl/subscriber.go
+++ pkg/statistics/handle/ddl/subscriber.go
@@ -230,6 +230,7 @@ func (h handler) handle(
 		}
 	case model.ActionFlashbackCluster:
 		return errors.Trace(storage.UpdateStatsVersion(ctx, sctx))
+	case model.ActionAddIndex:
 	default:
 		intest.Assert(false)
 		logutil.StatsLogger().Error("Unhandled schema change event",
